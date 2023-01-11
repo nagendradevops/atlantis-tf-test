c63f672c348d2e69400ead330eecba0336ab2435
Nagendra Reddy <bnreddi1985@gmail.com>
2023-01-11 19:22:18 +0530

    updated in test branch

--- c2-generic-variables.tf
+++ c2-generic-variables.tf
@@ -2,7 +2,7 @@
 
 # AWS Region
 variable "aws_region" {
-  description = "Region in which AWS Resources to be created"
+  description = "Region in which AWS Resources to be created testing"
   type        = string
   default     = "us-east-1"
 }
