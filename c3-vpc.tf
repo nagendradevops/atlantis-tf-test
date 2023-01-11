8938b4675876f1c2ebe0a6dc15d2e8f7ad7f08eb
Nagendra Reddy <bnreddi1985@gmail.com>
2023-01-11 07:38:02 +0530

    updated teh vpc cidrs

--- c3-vpc.tf
+++ c3-vpc.tf
@@ -6,9 +6,9 @@ module "vpc" {
 
   # VPC Basic Details
   name            = "vpc-dev"
-  cidr            = "10.0.0.0/16"
+  cidr            = "10.0.1.0/16"
   azs             = ["us-east-1a", "us-east-1b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
   # Database Subnets
@@ -40,7 +40,7 @@ module "vpc" {
   }
 
   tags = {
-    Owner       = "kalyan"
+    Owner       = "Nagendra"
     Environment = "dev"
   }
 
