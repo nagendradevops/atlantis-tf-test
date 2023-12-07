b9feb19cc5f80a1d1b8dcc3a744ec5d458c9824e
EC2 Default User <ec2-user@ip-172-31-44-196.ap-south-1.compute.internal>
2023-12-07 09:23:39 +0000

    changed the values in .tf file

--- proj1/c2-generic-variables.tf
+++ proj1/c2-generic-variables.tf
@@ -1,6 +1,8 @@
+# Input Variables
+
 # AWS Region
 variable "aws_region" {
-  description = "Region "
+  description = "Region ap-south-1 mumbai "
   type        = string
   default     = "us-east-1"
 }
