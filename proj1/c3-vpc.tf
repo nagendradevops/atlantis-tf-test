44495cd2b62bd87bf37f49160f4ae36c6ed24f31
EC2 Default User <ec2-user@ip-172-31-44-196.ap-south-1.compute.internal>
2023-12-07 07:19:19 +0000

    updated the vpc

--- proj1/c3-vpc.tf
+++ proj1/c3-vpc.tf
@@ -6,9 +6,9 @@ module "vpc" {
 
   # VPC Basic Details
   name            = "vpc-dev"
-  cidr            = "10.0.2.0/16"
+  cidr            = "10.0.1.0/16"
   azs             = ["us-east-1a", "us-east-1b"]
-  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+  private_subnets = ["10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
   # Database Subnets
